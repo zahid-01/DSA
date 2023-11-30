fe4d2d7337a4916bb07827ff89bb2d22cee67c5d
zahid-01 <erzahidkhan101@gmail.com>
2023-11-30 14:10:07 +0530

    dfs

--- Searching/DFS.js
+++ Searching/DFS.js
@@ -38,21 +38,7 @@ class myTree {
     }
   }
 
-  bfs() {
-    let que = new queue(this.root);
-    let top = que;
-    let tail = que;
-    while (true) {
-      console.log("--> ", top.node.value);
-      tail.next = top.node.left;
-      tail = tail.next;
-      tail.next = top.node.right;
-      tail = tail.next;
-      console.log(tail);
-      top = top.next;
-      if (!top) break;
-    }
-  }
+  bfs() {}
 }
 
 const tree = new myTree(9);
